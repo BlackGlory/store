2627d35f0dc0dcd7e2312ee75c70af7eba8c78d3
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-12-18 02:25:45 +0800

    test: add cases

---
+++ __tests__/services/stats/index.spec.ts
@@ -0,0 +1,30 @@
+import { buildServer } from '@src/server'
+import { resetDatabases, resetEnvironment } from '@test/utils'
+
+jest.mock('@dao/config-in-sqlite3/database')
+jest.mock('@dao/data-in-leveldb/database')
+
+beforeEach(async () => {
+  resetEnvironment()
+  await resetDatabases()
+})
+
+describe('stats', () => {
+  describe('GET /stats', () => {
+    it('200', async () => {
+      const server = await buildServer()
+
+      const res = await server.inject({
+        method: 'GET'
+      , url: '/stats'
+      })
+
+      expect(res.statusCode).toBe(200)
+      expect(res.json()).toMatchObject({
+        memoryUsage: expect.anything()
+      , cpuUsage: expect.anything()
+      , resourceUsage: expect.anything()
+      })
+    })
+  })
+})
