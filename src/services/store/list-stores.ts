ba40724de405354b84f08358de43d2cda7715198
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-12-31 13:00:11 +0800

    fix: body

--- src/services/store/list-stores.ts
+++ src/services/store/list-stores.ts
@@ -1,10 +1,11 @@
 import { FastifyPluginAsync } from 'fastify'
+import { toArrayAsync } from 'iterable-operator'
 
 export const routes: FastifyPluginAsync<{ Core: ICore }> = async function routes(server, { Core }) {
   server.get(
     '/store'
   , async (req, reply) => {
-      const result = await Core.Store.listStores()
+      const result = await toArrayAsync(Core.Store.listStores())
       reply.send(result)
     }
   )
