ddb8fcec74303520753f041ccbcb863e4a70e72e
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-01-01 10:47:04 +0800

    fix: process.on

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-import { prepareDatabase as prepareConfigInSqlite3Database } from '@src/dao/config-in-sqlite3/database'
-import { prepareDatabase as prepareDataInSqlite3Database } from '@src/dao/data-in-sqlite3/database'
+import {
+  prepareDatabase as prepareConfigInSqlite3Database
+, closeDatabase as closeConfigInSqlite3Database
+} from '@src/dao/config-in-sqlite3/database'
+import {
+  prepareDatabase as prepareDataInSqlite3Database
+, closeDatabase as closeDataInSqlite3Database
+} from '@src/dao/data-in-sqlite3/database'
 import { buildServer } from './server'
 import { PORT, HOST, CI } from '@env'
 
-process.on('SIGHUP', () => process.exit(1))
+process.on('exit', () => {
+  closeDataInSqlite3Database()
+  closeConfigInSqlite3Database()
+})
+process.on('SIGHUP', () => process.exit(128 + 1))
+process.on('SIGINT', () => process.exit(128 + 2))
+process.on('SIGTERM', () => process.exit(128 + 15))
 
 ;(async () => {
   await prepareConfigInSqlite3Database()
