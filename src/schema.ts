a57d15a06e1c5bc2b591b3ee496ae8bd4592dc4b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-01-05 11:41:06 +0800

    fix: schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,9 @@
 export const tokenSchema = {
   type: 'string'
-, pattern: '^[a-zA-Z0-9\\.\\-_]{1,256}$'
+, pattern: '^[a-zA-Z0-9\\.\\-_]{0,255}$'
 }
 
 export const idSchema = {
   type: 'string'
-, pattern: '^[a-zA-Z0-9\\.\\-_]{1,256}$'
+, pattern: '^[a-zA-Z0-9\\.\\-_]{0,255}$'
 }
