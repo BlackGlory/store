542a54ccce4640aa081701b3d0687aa620e2fdd2
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-01-29 03:40:23 +0000

    refactor: use extra-generator

--- src/services/store/list-items.ts
+++ src/services/store/list-items.ts
@@ -2,6 +2,7 @@ import { FastifyPluginAsync } from 'fastify'
 import { idSchema, tokenSchema } from '@src/schema'
 import accepts from 'fastify-accepts'
 import { Readable } from 'stream'
+import { stringifyJSONStreamAsync, stringifyNDJSONStreamAsync } from 'extra-generator'
 
 export const routes: FastifyPluginAsync<{ Core: ICore }> = async function routes(server, { Core }) {
   server.register(accepts)
@@ -17,57 +18,31 @@ export const routes: FastifyPluginAsync<{ Core: ICore }> = async function routes
       , querystring: { token: tokenSchema }
       }
     }
-  , (req, reply) => {
-      ;(async () => {
-        const storeId = req.params.storeId
-        const token = req.query.token
+  , async (req, reply) => {
+      const storeId = req.params.storeId
+      const token = req.query.token
 
-        try {
-          await Core.Blacklist.check(storeId)
-          await Core.Whitelist.check(storeId)
-          await Core.TBAC.checkReadPermission(storeId, token)
-        } catch (e) {
-          if (e instanceof Core.Blacklist.Forbidden) return reply.status(403).send()
-          if (e instanceof Core.Whitelist.Forbidden) return reply.status(403).send()
-          if (e instanceof Core.TBAC.Unauthorized) return reply.status(401).send()
-          throw e
-        }
+      try {
+        await Core.Blacklist.check(storeId)
+        await Core.Whitelist.check(storeId)
+        await Core.TBAC.checkReadPermission(storeId, token)
+      } catch (e) {
+        if (e instanceof Core.Blacklist.Forbidden) return reply.status(403).send()
+        if (e instanceof Core.Whitelist.Forbidden) return reply.status(403).send()
+        if (e instanceof Core.TBAC.Unauthorized) return reply.status(401).send()
+        throw e
+      }
 
-        const items = Core.Store.listItems(storeId)
+      const items = Core.Store.listItems(storeId)
 
-        const accept = req.accepts().type(['application/json', 'application/x-ndjson'])
-        if (accept === 'application/x-ndjson') {
-          reply.header('Content-Type', 'application/x-ndjson')
-          reply.send(Readable.from(generateNDJson(items)))
-        } else {
-          reply.header('Content-Type', 'application/json')
-          reply.send(Readable.from(generateJSON(items)))
-        }
-      })()
+      const accept = req.accepts().type(['application/json', 'application/x-ndjson'])
+      if (accept === 'application/x-ndjson') {
+        reply.header('Content-Type', 'application/x-ndjson')
+        reply.send(Readable.from(stringifyNDJSONStreamAsync(items)))
+      } else {
+        reply.header('Content-Type', 'application/json')
+        reply.send(Readable.from(stringifyJSONStreamAsync(items)))
+      }
     }
   )
-
-  async function* generateNDJson(asyncIterable: AsyncIterable<string>): AsyncIterable<string> {
-    const iter = asyncIterable[Symbol.asyncIterator]()
-    const firstResult = await iter.next()
-    if (!firstResult.done) yield JSON.stringify(firstResult.value)
-    while (true) {
-      const result = await iter.next()
-      if (result.done) break
-      yield '\n' + JSON.stringify(result.value)
-    }
-  }
-
-  async function* generateJSON(asyncIterable: AsyncIterable<string>): AsyncIterable<string> {
-    const iter = asyncIterable[Symbol.asyncIterator]()
-    const firstResult = await iter.next()
-    yield '['
-    if (!firstResult.done) yield JSON.stringify(firstResult.value)
-    while (true) {
-      const result = await iter.next()
-      if (result.done) break
-      yield ',' + JSON.stringify(result.value)
-    }
-    yield ']'
-  }
 }
