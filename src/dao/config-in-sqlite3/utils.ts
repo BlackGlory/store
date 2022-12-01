32dce02256c750d52827701fea80b3107ab8150e
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-12-01 09:12:41 +0000

    chore: upgrade dependencies

--- src/dao/config-in-sqlite3/utils.ts
+++ src/dao/config-in-sqlite3/utils.ts
@@ -1,11 +1,15 @@
 import * as path from 'path'
 import type { Database as IDatabase } from 'better-sqlite3'
 import { path as appRoot } from 'app-root-path'
-import { readMigrations } from 'migrations-file'
+import { readMigrationFile, findMigrationFilenames } from 'migration-files'
+import { map } from 'extra-promise'
 import { migrate } from '@blackglory/better-sqlite3-migrations'
 
 export async function migrateDatabase(db: IDatabase): Promise<void> {
   const migrationsPath = path.join(appRoot, 'migrations/config-in-sqlite3')
-  const migrations = await readMigrations(migrationsPath)
+  const migrations = await map(
+    await findMigrationFilenames(migrationsPath)
+  , readMigrationFile
+  )
   migrate(db, migrations)
 }
